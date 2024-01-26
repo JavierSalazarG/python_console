407890019c007d63a05fa8ea13a878d842556865
¨javi <javis9555@gmail.com>
2024-01-26 11:11:34 +0100

    fix comments update terminado

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ def runApp():
     parser = argparse.ArgumentParser(
         prog="Python app",
         description="Datos de JSON")
-    parser.add_argument("action", choices=['list_users', "view_users", "view_bookings", "list_bookings", "list_rooms" , "view_rooms", "list_comments", "view_comments"])
+    parser.add_argument("action", choices=['list_users', "view_users","delete_users","view_bookings", "list_bookings", "delete_bookings" "list_rooms" , "view_rooms","delete_rooms", "list_comments", "view_comments", "delete_comments", "create_comments", "update_comments"])
     
     argumentos = parser.parse_args()
 
@@ -13,16 +13,28 @@ def runApp():
         Users.list()
     elif argumentos.action == 'view_users':
         Users.view()
+    elif argumentos.action == 'delete_users':
+        Users.delete()
     elif argumentos.action == 'list_bookings':
         Bookings.list()
     elif argumentos.action == 'view_bookings':
         Bookings.view()
+    elif argumentos.action == 'delete_bookings':
+        Bookings.delete()
     elif argumentos.action == 'list_rooms':
         Rooms.list()
+    elif argumentos.action == 'delete_rooms':
+        Rooms.delete()
     elif argumentos.action == 'view_rooms':
         Rooms.view()
     elif argumentos.action == 'list_comments':
         Comments.list()
     elif argumentos.action == 'view_comments':
         Comments.view()
+    elif argumentos.action == 'delete_comments':
+        Comments.delete()
+    elif argumentos.action == "create_comments":
+        Comments.create()
+    elif argumentos.action == "update_comments":
+        Comments.update()
 runApp()
--- src/models.py
+++ src/models.py
@@ -21,6 +21,68 @@ class Model(ABC):
         for element in data:
             if element["_id"] == int(id):
                 print(element)
+    @classmethod
+    def delete(cls):
+        with open(cls.path, 'r') as file:
+            data = json.load(file)
+        print('Introduce id para eliminar:')
+        id = int(input())
+        elements = [element for element in data if element['_id'] != id]
+        if any(element["_id"] == id for element in data):
+            print(f"el id {id} se ha eliminado correctamente")
+        else:
+            print('id no encontrado')
+        for element in elements:
+            print(element)
+
+    @classmethod
+    def create(cls):
+        with open(cls.path) as file:
+            data = json.load(file)
+            l_data = len(data)
+
+            fecha = datetime.now()
+            new_comments = {
+                "_id": l_data + 1,
+                "nombre": input("introduce tu nombre:"),
+                "fecha": fecha,
+                "foto_perfil": "https://loremflickr.com/cache/resized/65535_52630952123_7666ab4252_b_640_480_nofilter.jpg",
+                "comentario": input("escribe tu comentario:"),
+                "archive": False
+            }
+            respuesta_archive = input("¿Deseas archivar el comentario? (responde 'si' o 'no'): ").lower()
+
+            if respuesta_archive == 'si':
+                new_comments["archive"] = True
+
+            print("se a creado con exito el comentario!")
+            print(new_comments)
+
+    @classmethod
+    def update(cls):
+         with open(cls.path) as file:
+            data = json.load(file)
+            print('Introduce id')
+            id = int(input())
+
+            for comment in data:
+                if comment["_id"] == id:
+                    print("comentario elegido:")
+                    print(comment)
+
+                    respuesta_actualizar = input("¿Deseas actualizar el comentario? (responde 'si' o 'no'): ").lower()
+
+                    if respuesta_actualizar == 'si':
+                        comment["nombre"] = input("introduce el Nombre nuevo")
+                        comment["comentario"] = input("introduce el comentario Nuevo ")
+                        print("Comentario actualizado:")
+                        print(comment)
+                    else:
+                        print("Comentario no actualizado")
+                    return
+                print(f"el comentario con el id {id} no se ha encontrado")
+
+            
 
 class Users(Model):
     path = os.path.join(os.path.dirname(__file__), '../data/user.json')
