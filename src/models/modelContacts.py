938f9bc33a02190f4ccedf358ec7051282759306
¨javi <javis9555@gmail.com>
2024-02-01 23:45:35 +0100

    update contacts

--- src/models/modelContacts.py
+++ src/models/modelContacts.py
@@ -48,3 +48,40 @@ class ModelContacts(Model):
         finally:
           
             close_connection(connection)
+    
+    def update(self):
+        connection = create_connection()
+        cursor = connection.cursor(dictionary=True)
+        
+        while True:
+            id_contact = input('Introduce el id del contacto que deseas actualizar: ')
+            query = f"SELECT * FROM contacts WHERE _id = '{id_contact}'"
+            cursor.execute(query)
+            data = cursor.fetchone()
+
+            if data:
+                break
+            else:
+                print(f"No se encontró el id {id_contact}. Inténtalo de nuevo.")
+
+        columns = list(data.keys())
+        columns.remove('email')
+        print("Selecciona la columna que deseas actualizar:")
+        for i, column in enumerate(columns, start=1):
+            print(f"{i}. {column}")
+
+        while True:
+            try:
+                selected_column_number = int(input("Ingresa el número correspondiente a la columna que deseas actualizar: "))
+                selected_column = columns[selected_column_number - 1]
+                break
+            except (ValueError, IndexError):
+                print("Error: Ingresa un número válido de la lista.")
+
+        new_value = input(f'Introduce el nuevo valor para {selected_column}: ')
+        update_query = f"UPDATE contacts SET {selected_column} = %s WHERE _id = %s"
+        cursor.execute(update_query, (new_value, id_contact))
+        connection.commit()
+        print(f"Se actualizó la columna {selected_column} con el nuevo valor: {new_value} para el usuario con id {id_contact}")
+
+        close_connection(connection)
